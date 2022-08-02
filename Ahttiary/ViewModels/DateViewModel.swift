f9be7a8b81175040bde4eb922c39a005becd30e2
phainestha1 <250wons@gmail.com>
2022-08-02 12:29:15 +0900

    [Add] DateViewModel

---
+++ Ahttiary/ViewModels/DateViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  DateViewModel.swift
+//  Ahttiary
+//
+//  Created by Noah's Ark on 2022/08/02.
+//
+
+import Foundation
+
+class DateViewModel: ObservableObject {
+    @Published var date: Date = Date()
+}
