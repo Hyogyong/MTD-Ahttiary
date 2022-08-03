b822d4cd2d032b831e1e2282c24ffd8258ee679d
seodam-hst <seodam.hst@gmail.com>
2022-08-03 13:44:11 +0900

    feat: 노트 작성 UI 생성

---
+++ Ahttiary/Views/WriteNoteView.swift
@@ -0,0 +1,77 @@
+//
+//  WriteNoteView.swift
+//  Ahttiary
+//
+//  Created by 임성균 on 2022/08/03.
+//
+
+import SwiftUI
+
+struct WriteNoteView: View {
+    @ObservedObject var noteManager = NoteManager()
+    
+    var body: some View {
+        ZStack {
+            Color.Custom.backgroundColor
+                .ignoresSafeArea()
+            
+            VStack {
+                // 아띠와 말풍선
+                HStack(alignment: .center) {
+                    Image("AhttyWriter")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: ScreenSize.ahttyWriterWidth)
+                    Text(NoteManager.questions[noteManager.pageNumber])
+                        .frame(
+                            height: ScreenSize.questionMessageBoxHeight,
+                            alignment: .center
+                        )
+                        .background(Color.white)
+                        .cornerRadius(15)
+                        .padding()
+                }
+                
+                HStack {
+                    Text(DateFormatter.getKoreanDateInString())
+                    Spacer()
+                    Text("맑음")
+                }
+                .padding(.horizontal)
+                .font(.callout)
+                .foregroundColor(.gray)
+                
+                // 노트 작성란
+                HStack {
+                    ZStack {
+                        TextEditor(text: $noteManager.answers[noteManager.pageNumber])
+                            .frame(
+                                height: ScreenSize.answerMessageBoxHeight
+                            )
+                            .cornerRadius(15)
+                            .padding()
+                    }
+                }
+                
+                HStack {
+                    Button("이전") {
+                        noteManager.goToPreviousPage()
+                    }
+                    .buttonStyle(.bordered)
+                    Button("다음") {
+                        noteManager.goToNextPage()
+                    }
+                    .buttonStyle(.bordered)
+                }
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+struct WriteNoteView_Previews: PreviewProvider {
+    static var previews: some View {
+        WriteNoteView()
+    }
+}
