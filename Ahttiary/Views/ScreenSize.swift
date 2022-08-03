2e1a4b1ab5504c7ecb496d6da66c1c359597fcf0
seodam-hst <seodam.hst@gmail.com>
2022-08-04 01:43:22 +0900

    refactor: 변수를 알파벳 순서대로 정렬

--- Ahttiary/Views/ScreenSize.swift
+++ Ahttiary/Views/ScreenSize.swift
@@ -9,17 +9,16 @@ import UIKit
 
 enum ScreenSize {
     // Device Screen Size
-    static var fullWidth: CGFloat { UIScreen.main.fixedCoordinateSpace.bounds.width }
     static var fullHeight: CGFloat { UIScreen.main.fixedCoordinateSpace.bounds.height }
+    static var fullWidth: CGFloat { UIScreen.main.fixedCoordinateSpace.bounds.width }
     
-    // WritingNoteView
+    // WriteNoteView
     static var ahttyWriterWidth: CGFloat { ScreenSize.fullWidth * (1 / 3) }
-    
-    static var questionMessageBoxWidth: CGFloat { ScreenSize.fullWidth * (2 / 3) }
-    static var questionMessageBoxHeight: CGFloat { questionMessageBoxWidth * (1 / 2) }
-    
-    static var answerMessageBoxWidth: CGFloat { ScreenSize.fullWidth * (1) }
+        
     static var answerMessageBoxHeight: CGFloat { answerMessageBoxWidth }
+    static var answerMessageBoxWidth: CGFloat { ScreenSize.fullWidth * (1) }
     
+    static var questionMessageBoxHeight: CGFloat { questionMessageBoxWidth * (1 / 2) }
+    static var questionMessageBoxWidth: CGFloat { ScreenSize.fullWidth * (2 / 3) }
 }
 
