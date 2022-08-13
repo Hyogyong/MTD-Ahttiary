497eb2dfbf001a06dd3a583abfe17b7f019b5f86
Phainestha1 <250wons@gmail.com>
2022-08-13 21:12:12 +0900

    [Add] Custom Fonts

---
+++ Ahttiary/Fonts/Font.swift
@@ -0,0 +1,28 @@
+//
+//  Font.swift
+//  Ahttiary
+//
+//  Created by Hyeon-sang Lee on 2022/08/12.
+//
+
+import Foundation
+
+struct Font {
+    static let shared = Font()
+    let comment = "나눔손글씨 힘내라는 말보단"
+    let calendarBold = "GangwonEduAll-OTFBold"
+    let calendarLight = "GangwonEduAll-OTFLight"
+}
+
+// 폰트가 적용되지 않는 경우 해당 Text에 아래 내용을 적용한 후
+// 출력되는 결과 중 올바른 폰트명을 확인하세요.
+/*
+.onAppear {
+    for family: String in UIFont.familyNames {
+        print(family)
+        for names : String in UIFont.fontNames(forFamilyName: family){
+            print("=== \(names)")
+        }
+    }
+}
+*/
