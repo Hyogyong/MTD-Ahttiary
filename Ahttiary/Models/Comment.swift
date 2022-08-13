93dbaaf9d508af4a054f089d12102e015d231b5c
Phainestha1 <250wons@gmail.com>
2022-08-13 21:13:17 +0900

    [Add] Comment file in Models folder

---
+++ Ahttiary/Models/Comment.swift
@@ -0,0 +1,19 @@
+//
+//  Comment.swift
+//  Ahttiary
+//
+//  Created by Hyeon-sang Lee on 2022/08/12.
+//
+
+import Foundation
+
+struct Comment {
+    static let shared = Comment()
+    
+    let mainComment: [String] = [
+        """
+        기다리던 시간이야!
+        오늘의 감정기록을 만들어보자.
+        """
+    ]
+}
