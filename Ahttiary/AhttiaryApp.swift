1315a58f4c615ea0cc355d5b6a434c5db45459a9
Hyeon-sang "Theo" Lee <88080251+phainestha1@users.noreply.github.com>
2022-08-15 23:35:08 +0900

    Revert "[FIX] pbxproj 손상"

--- Ahttiary/AhttiaryApp.swift
+++ Ahttiary/AhttiaryApp.swift
@@ -10,14 +10,12 @@ import SwiftUI
 @main
 struct AhttiaryApp: App {
     
-    @StateObject var persistentStore = PersistentStore.shared
     @StateObject var dateViewModel: DateViewModel = DateViewModel()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(dateViewModel)
-                .environment(\.managedObjectContext, persistentStore.context)
         }
     }
 }
