54b82bf6c5a7d4ceb14ddfb9eeb837f01a70a490
seodam-hst <seodam.hst@gmail.com>
2022-08-16 00:28:55 +0900

    feature/writing 브랜치의 변경 내역을 feature/main 브랜치로 이동

---
+++ Ahttiary/Views/Note/WriteNoteView.swift
@@ -0,0 +1,63 @@
+//
+//  WriteNoteView.swift
+//  Ahttiary
+//
+//  Created by 임성균 on 2022/08/03.
+//
+
+import SwiftUI
+
+struct WriteNoteView: View {
+    
+    @ObservedObject var noteManager: NoteManager = NoteManager()
+    @StateObject private var draftNote: DraftNote
+    
+    init(note: Note) {
+        _draftNote = StateObject(wrappedValue: DraftNote(note: note))
+    }
+    
+    var body: some View {
+        
+        TabView(selection: $noteManager.pageNumber) {
+            // 상황
+            WritePageView(noteManager: noteManager, answer: $draftNote.firstAnswer)
+                .tag(0)
+            
+            // 정서
+            WritePageView(noteManager: noteManager, answer: $draftNote.secondAnswer)
+                .tag(1)
+            
+            // 첫 번째 감정 체크
+            CheckPageView(noteManager: noteManager)
+                .tag(2)
+            
+            // 자동적 사고 기술
+            WritePageView(noteManager: noteManager, answer: $draftNote.thirdAnswer)
+                .tag(3)
+            
+            // 인지 왜곡 파악
+            WritePageView(noteManager: noteManager, answer: $draftNote.fourthAnswer)
+                .tag(4)
+            
+            // 합리적 반응 도출
+            WritePageView(noteManager: noteManager, answer: $draftNote.fifthAnswer)
+                .tag(5)
+            
+            // 두 번째 감정 체크
+            CheckPageView(noteManager: noteManager)
+                .tag(6)
+            
+            // 마지막 페이지(내일도 즐거운 하루 보내자!)
+            EndPageView(noteManager: noteManager)
+                .tag(7)
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
+        .ignoresSafeArea()
+        .navigationBarTitleDisplayMode(.inline)
+        .onDisappear {
+            Note.updateNote(using: draftNote)
+            noteManager.goToFirstPage()
+        }
+        
+    } // End of body
+}
