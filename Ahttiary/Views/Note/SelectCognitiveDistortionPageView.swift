2079185de1949c64be594e05aee3e64a1201fe3a
seodam-hst <seodam.hst@gmail.com>
2022-08-24 16:29:28 +0900

    create SelectCognitiveDistortionPageView

---
+++ Ahttiary/Views/Note/SelectCognitiveDistortionPageView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectCognitiveDistortionPageView.swift
+//  Ahttiary
+//
+//  Created by 임성균 on 2022/08/24.
+//
+
+import SwiftUI
+
+struct SelectCognitiveDistortionPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectCognitiveDistortionPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectCognitiveDistortionPageView()
+    }
+}
