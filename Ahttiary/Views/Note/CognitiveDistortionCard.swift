377eca8d53975bb7195b777eb2ac4c85b0afba2e
seodam-hst <seodam.hst@gmail.com>
2022-08-24 19:31:25 +0900

    인지 왜곡에 대한 설명과 예시를 담고 있는 추가 설명 카드 추가

---
+++ Ahttiary/Views/Note/CognitiveDistortionCard.swift
@@ -0,0 +1,20 @@
+//
+//  CognitiveDistortionCard.swift
+//  Ahttiary
+//
+//  Created by 임성균 on 2022/08/24.
+//
+
+import SwiftUI
+
+struct CognitiveDistortionCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CognitiveDistortionCard_Previews: PreviewProvider {
+    static var previews: some View {
+        CognitiveDistortionCard()
+    }
+}
