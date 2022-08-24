b6b530461ff0850e43f96c387645635680afc2f0
seodam-hst <seodam.hst@gmail.com>
2022-08-24 22:39:31 +0900

    DistortionCardManager 반영

--- Ahttiary/Views/Note/SelectCognitiveDistortionPageView.swift
+++ Ahttiary/Views/Note/SelectCognitiveDistortionPageView.swift
@@ -12,75 +12,85 @@ struct SelectCognitiveDistortionPageView: View {
     @ObservedObject var noteManager: NoteManager
     @Binding var answer: String
     
+    @ObservedObject var distortionCardManager: DistortionCardManager = DistortionCardManager()
+    
     var body: some View {
-        VStack {
-            // 아띠와 말풍선
-            HStack(alignment: .center) {
-                Image("ahttyHello")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: ScreenSize.ahttyWriterWidth)
-                
-                Text(noteManager.randomComments[noteManager.pageNumber])
-                    .frame(
-                        height: ScreenSize.questionMessageBoxHeight,
-                        alignment: .center
-                    )
-                    .background(Color.Custom.background)
-                    .font(.custom(Font.shared.comment, size: 20))
-                    .padding()
-            }
-            
-            // Emotion Selecting Buttons
-            ScrollView {
-                VStack {
-                    HStack {
-                        CognitiveDistortionCell(type: .polarizedThinking, answer: $answer)
-                        
-                        CognitiveDistortionCell(type: .overgeneralization, answer: $answer)
-                    }
+        ZStack {
+            VStack {
+                // 아띠와 말풍선
+                HStack(alignment: .center) {
+                    Image("ahttyHello")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: ScreenSize.ahttyWriterWidth)
                     
-                    HStack {
-                        CognitiveDistortionCell(type: .catastrophizing, answer: $answer)
-                        
-                        CognitiveDistortionCell(type: .personalization, answer: $answer)
-                    }
-                    
-                    HStack {
-                        CognitiveDistortionCell(type: .mindReading, answer: $answer)
-                        
-                        CognitiveDistortionCell(type: .mentalFiltering, answer: $answer)
-                    }
-                    
-                    HStack {
-                        CognitiveDistortionCell(type: .discountingThePositive, answer: $answer)
-                        
-                        CognitiveDistortionCell(type: .shouldStatements, answer: $answer)
-                    }
-                    
-                    HStack {
-                        CognitiveDistortionCell(type: .emotionalReasoning, answer: $answer)
-                        
-                        CognitiveDistortionCell(type: .labeling, answer: $answer)
-                    }
-                }
-            }
-            
-            HStack(spacing: 20) {
-                CustomButton("이전") {
-                    noteManager.goToPreviousPage()
+                    Text(noteManager.randomComments[noteManager.pageNumber])
+                        .frame(
+                            height: ScreenSize.questionMessageBoxHeight,
+                            alignment: .center
+                        )
+                        .background(Color.Custom.background)
+                        .font(.custom(Font.shared.comment, size: 20))
+                        .padding()
                 }
                 
-                CustomButton("선택 완료") {
-                    noteManager.goToNextPage()
+                // Emotion Selecting Buttons
+                ScrollView {
+                    VStack {
+                        HStack {
+                            CognitiveDistortionCell(type: .polarizedThinking, answer: $answer, distortionCardManager: distortionCardManager)
+                            
+                            CognitiveDistortionCell(type: .overgeneralization, answer: $answer, distortionCardManager: distortionCardManager)
+                        }
+                        
+                        HStack {
+                            CognitiveDistortionCell(type: .catastrophizing, answer: $answer, distortionCardManager: distortionCardManager)
+                            
+                            CognitiveDistortionCell(type: .personalization, answer: $answer, distortionCardManager: distortionCardManager)
+                        }
+                        
+                        HStack {
+                            CognitiveDistortionCell(type: .mindReading, answer: $answer, distortionCardManager: distortionCardManager)
+                            
+                            CognitiveDistortionCell(type: .mentalFiltering, answer: $answer, distortionCardManager: distortionCardManager)
+                        }
+                        
+                        HStack {
+                            CognitiveDistortionCell(type: .discountingThePositive, answer: $answer, distortionCardManager: distortionCardManager)
+                            
+                            CognitiveDistortionCell(type: .shouldStatements, answer: $answer, distortionCardManager: distortionCardManager)
+                        }
+                        
+                        HStack {
+                            CognitiveDistortionCell(type: .emotionalReasoning, answer: $answer, distortionCardManager: distortionCardManager)
+                            
+                            CognitiveDistortionCell(type: .labeling, answer: $answer, distortionCardManager: distortionCardManager)
+                        }
+                    }
                 }
-                .disabled(answer.isEmpty)
-                .opacity(answer.isEmpty ? 0.7 : 1)
+                
+                HStack(spacing: 20) {
+                    CustomButton("이전") {
+                        noteManager.goToPreviousPage()
+                    }
+                    
+                    CustomButton("선택 완료") {
+                        noteManager.goToNextPage()
+                    }
+                    .disabled(answer.isEmpty)
+                    .opacity(answer.isEmpty ? 0.7 : 1)
+                }
+                
+                Spacer()
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.Custom.background.ignoresSafeArea())
             
-            Spacer()
+            if distortionCardManager.isShowingDistortionCard {
+                Color.black.opacity(0.2)
+                CognitiveDistortionCard(distortionCardManager: distortionCardManager)
+                    .frame(width: ScreenSize.distortionCardWidth, height: ScreenSize.distortionCardHeight, alignment: .center)
+            }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.Custom.background.ignoresSafeArea())
     }
 }
