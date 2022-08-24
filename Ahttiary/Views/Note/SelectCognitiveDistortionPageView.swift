581856e88dd529532782acef6ca7208516a2b100
seodam-hst <seodam.hst@gmail.com>
2022-08-24 19:32:56 +0900

    인지 왜곡 선택 페이지 UI 구성

--- Ahttiary/Views/Note/SelectCognitiveDistortionPageView.swift
+++ Ahttiary/Views/Note/SelectCognitiveDistortionPageView.swift
@@ -8,13 +8,79 @@
 import SwiftUI
 
 struct SelectCognitiveDistortionPageView: View {
+    
+    @ObservedObject var noteManager: NoteManager
+    @Binding var answer: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct SelectCognitiveDistortionPageView_Previews: PreviewProvider {
-    static var previews: some View {
-        SelectCognitiveDistortionPageView()
+        VStack {
+            // 아띠와 말풍선
+            HStack(alignment: .center) {
+                Image("ahttyHello")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: ScreenSize.ahttyWriterWidth)
+                
+                Text(noteManager.randomComments[noteManager.pageNumber])
+                    .frame(
+                        height: ScreenSize.questionMessageBoxHeight,
+                        alignment: .center
+                    )
+                    .background(Color.Custom.background)
+                    .font(.custom(Font.shared.comment, size: 20))
+                    .padding()
+            }
+            
+            // Emotion Selecting Buttons
+            ScrollView {
+                VStack {
+                    HStack {
+                        CognitiveDistortionCell(type: .polarizedThinking, answer: $answer)
+                        
+                        CognitiveDistortionCell(type: .overgeneralization, answer: $answer)
+                    }
+                    
+                    HStack {
+                        CognitiveDistortionCell(type: .catastrophizing, answer: $answer)
+                        
+                        CognitiveDistortionCell(type: .personalization, answer: $answer)
+                    }
+                    
+                    HStack {
+                        CognitiveDistortionCell(type: .mindReading, answer: $answer)
+                        
+                        CognitiveDistortionCell(type: .mentalFiltering, answer: $answer)
+                    }
+                    
+                    HStack {
+                        CognitiveDistortionCell(type: .discountingThePositive, answer: $answer)
+                        
+                        CognitiveDistortionCell(type: .shouldStatements, answer: $answer)
+                    }
+                    
+                    HStack {
+                        CognitiveDistortionCell(type: .emotionalReasoning, answer: $answer)
+                        
+                        CognitiveDistortionCell(type: .labeling, answer: $answer)
+                    }
+                }
+            }
+            
+            HStack(spacing: 20) {
+                CustomButton("이전") {
+                    noteManager.goToPreviousPage()
+                }
+                
+                CustomButton("선택 완료") {
+                    noteManager.goToNextPage()
+                }
+                .disabled(answer.isEmpty)
+                .opacity(answer.isEmpty ? 0.7 : 1)
+            }
+            
+            Spacer()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.Custom.background.ignoresSafeArea())
     }
 }
