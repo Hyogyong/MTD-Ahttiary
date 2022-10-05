e44a8a35a024c72463dbaad6276b300a002f6214
Hyogyong <lhglhs98@gmail.com>
2022-10-06 02:06:00 +0900

    feat: 앱을 처음으로 실행하였을 때 컨텐츠 뷰에 온보딩이 나타나게 했습니다.

--- Ahttiary/Views/ContentView.swift
+++ Ahttiary/Views/ContentView.swift
@@ -12,9 +12,13 @@ struct ContentView: View {
     @EnvironmentObject var dateManager: DateViewModel
     @FetchRequest(fetchRequest: Note.allNotesFR())
     private var notes: FetchedResults<Note>
+    @AppStorage("_isFirstLaunching") var isFirstLaunching: Bool = true
     
     var body: some View {
         contentView
+            .fullScreenCover(isPresented: $isFirstLaunching) {
+                OnboardingTabView(isFirstLaunching: $isFirstLaunching)
+            }
     }// body
     
     @ViewBuilder private var contentView: some View {
