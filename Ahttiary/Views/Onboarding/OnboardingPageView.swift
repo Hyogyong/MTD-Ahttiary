6b1ea921a04bfaf8ed260ae98a93be7291b2d59b
Hyogyong <lhglhs98@gmail.com>
2022-10-06 02:03:10 +0900

    feat: 온보딩에 들어갈 개별 페이지를 만들었습니다.

---
+++ Ahttiary/Views/Onboarding/OnboardingPageView.swift
@@ -0,0 +1,41 @@
+//
+//  OnboardingPageView.swift
+//  아띠어리
+//
+//  Created by 이효경 on 2022/10/06.
+//
+
+import SwiftUI
+
+struct OnboardingPageView: View {
+    let imageName: String
+    let title: String
+    let subtitle: String
+    
+    var body: some View {
+        VStack {
+            Image(imageName)
+                .font(.system(size: 100))
+                .padding()
+            
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+            
+            Text(subtitle)
+                .font(.title2)
+        }
+        
+    }
+}
+
+struct OnboardingPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingPageView(
+            imageName: "note.text.badge.plus",
+            title: "쓰기 탭",
+            subtitle: "이 앱은 개인 메모장으로 쓸 수 있어요"
+        )
+    }
+}
